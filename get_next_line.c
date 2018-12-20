56d1de7edd48ae1d61d2cc538e3cef236130e3ba
ssfar <ssfar@student.42.fr>
2018-12-20 21:28:50 +0100

    deleted .o , .a and added fd < 0 condition in gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,15 @@
 /*   By: ssfar <ssfar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 18:14:06 by ssfar             #+#    #+#             */
-/*   Updated: 2018/12/03 02:56:49 by ssfar            ###   ########.fr       */
+/*   Updated: 2018/12/07 10:18:14 by ssfar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_glist		*ft_glstnew(char *s, int fd, t_glist *first)
+static t_glist		*ft_glstnew(char *s, int fd, t_glist *first)
 {
-	t_glist	*list;
+	t_glist			*list;
 
 	if (!(list = (t_glist*)malloc(sizeof(t_glist))))
 		return (NULL);
@@ -24,9 +24,9 @@ t_glist		*ft_glstnew(char *s, int fd, t_glist *first)
 	return (list);
 }
 
-int			get_line(t_glist **first, t_glist *lst, char **line)
+static int			get_line(t_glist **first, t_glist *lst, char **line)
 {
-	t_glist *prev;
+	t_glist			*prev;
 
 	if (!(*line = ft_strsub(lst->s, 0, ft_strclen(lst->s, '\n'))))
 		return (-1);
@@ -49,33 +49,31 @@ int			get_line(t_glist **first, t_glist *lst, char **line)
 	return (1);
 }
 
-int			fill_lst(t_glist **first, t_glist **lst, int fd, char *buf)
+static int			fill_lst(t_glist **first, t_glist **lst, int fd, char *buf)
 {
-	int		ret;
-	char	*to_free;
+	int				ret;
+	char			*to_free;
 
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		if (!*lst)
 		{
-			if (!(*lst = ft_glstnew(ft_strdup(buf), fd, *first)))
+			if (!(*lst = ft_glstnew(ft_strnew(0), fd, *first)))
 				return (0);
 			*first = *lst;
 		}
-		else if ((to_free = (*lst)->s))
-		{
-			if (!((*lst)->s = ft_strjoin((*lst)->s, buf)))
-				return (0);
-			free(to_free);
-		}
+		to_free = (*lst)->s;
+		if (!((*lst)->s = ft_strjoin((*lst)->s, buf)))
+			return (0);
+		free(to_free);
 		if (ft_strchr((*lst)->s, '\n'))
 			break ;
 	}
 	return (1);
 }
 
-int			get_next_line(const int fd, char **line)
+int					get_next_line(const int fd, char **line)
 {
 	static t_glist	*first = NULL;
 	t_glist			*lst;
@@ -84,7 +82,7 @@ int			get_next_line(const int fd, char **line)
 	lst = first;
 	while (lst && lst->fd != fd)
 		lst = lst->next;
-	if (!line || BUFF_SIZE <= 0 || read(fd, buf, 0) < 0
+	if (!line || BUFF_SIZE <= 0 || fd < 0 || read(fd, buf, 0) < 0
 	|| !fill_lst(&first, &lst, fd, buf))
 		return (-1);
 	return (lst && ft_strlen(lst->s) ? get_line(&first, lst, line) : 0);
